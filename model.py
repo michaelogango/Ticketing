323a0bd2be00a280ee012969b63611e60faa2559
Michael Odhiambo <michaelogango5@gmail.com>
2025-02-03 07:55:54 +0300

    render

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-# from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy_serializer import SerializerMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 import os
 
